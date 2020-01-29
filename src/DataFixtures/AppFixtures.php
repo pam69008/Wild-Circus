749a21eff704f13df489fb6ed5078be5687a1131
Pierre-Alain Monney <pierrealain.monney@gmail.com>
2020-01-29 13:51:16 +0100

    page membre terminée

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Team;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $faker = Faker\Factory::create('fr_FR');
+
+        for ($i = 0; $i < 10; $i++) {
+
+            $member = new Team();
+            $member->setFirstname($faker->firstName);
+            $member->setLastname($faker->lastName);
+            $member->setDescription($faker->text);
+            $manager->persist($member);
+        }
+
+        $manager->flush();
+    }
+}
