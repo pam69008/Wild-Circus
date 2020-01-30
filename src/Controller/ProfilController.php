d9638dc121488e5768feace3c37c29b8defeebb0
Pierre-Alain Monney <pierrealain.monney@gmail.com>
2020-01-30 11:39:30 +0100

    ajout historique de commande dans le profil

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Inscription;
+use App\Repository\InscriptionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,7 +15,12 @@ class ProfilController extends AbstractController
     public function index()
     {
         $user = $this->getUser();
-        return $this->render('profil/index.html.twig', ['user'=>$user
+        $inscription = $this->getDoctrine()
+            ->getRepository(Inscription::class)
+            ->findBy(['user'=>$user]);
+
+        return $this->render('profil/index.html.twig', ['user' => $user,
+            'inscriptions' => $inscription
         ]);
     }
 }
